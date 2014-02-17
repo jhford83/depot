5b4f2e4ae969e90480f96691dffbaf310ba4c65f
jhford83 <jhford@dsu.edu>
2014-02-17 04:44:49 -0600

    Ch. 8 Catalog Display
    Finished Task C

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ActiveRecord::Base
+  default_scope :order => 'title'
+
+  # validation stuff...
+
+
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
 # 
@@ -7,4 +12,5 @@ class Product < ActiveRecord::Base
     :with    => %r{\.(gif|jpg|png)\z}i,
     :message => 'must be a URL for GIF, JPG or PNG image.'
   }
+  validates :title, :length => {:minimum => 10}
 end
