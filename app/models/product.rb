3093d3c224f5d08a4e8451a45bc5bd71f0f23a2e
jhford83 <jhford@dsu.edu>
2014-02-25 01:55:56 -0600

    Ch. 9
    Task D: Cart Creation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,11 @@
 class Product < ActiveRecord::Base
   default_scope :order => 'title'
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  #...
+
 
   # validation stuff...
 
@@ -13,4 +19,15 @@ class Product < ActiveRecord::Base
     :message => 'must be a URL for GIF, JPG or PNG image.'
   }
   validates :title, :length => {:minimum => 10}
+  private
+
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line Items present')
+        return false
+      end
+    end
 end
